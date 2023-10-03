56e262dfd779de47f227e4c78d69074e95fcb739
Pavel <admaikinpavel@yandex.ru>
2023-10-03 17:42:50 +0300

    fix: fix route creation logic

--- src/Lab1/Environments/Entities/Route.cs
+++ src/Lab1/Environments/Entities/Route.cs
@@ -1,44 +1,14 @@
-using System;
-using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Itmo.ObjectOrientedProgramming.Lab1.Environments.Entities;
 
 public class Route
 {
-    public ArrayList Segments { get; } = new ArrayList();
+    public Collection<Environment> Segments { get; } = new(new List<Environment>().AsReadOnly());
 
-    public void CreateRoute(int segmentsCount, int[][] requirements)
+    public void AddSegment(Environment environment)
     {
-        for (int i = 0; i < segmentsCount; i++)
-        {
-            if (requirements != null)
-            {
-                int[] requirement = requirements[i];
-                int env = requirement[0];
-                int segmentLength = requirement[1];
-                int asteroids = requirement[2];
-                int meteors = requirement[3];
-                int flashes = requirement[4];
-                int whales = requirement[5];
-                Environment newEnvironment;
-                switch (env)
-                {
-                    case 1:
-                        newEnvironment = new Space(asteroids, meteors, segmentLength);
-                        Segments.Add(newEnvironment);
-                        break;
-                    case 2:
-                        newEnvironment = new HighDensityNebulae(flashes, segmentLength);
-                        Segments.Add(newEnvironment);
-                        break;
-                    case 3:
-                        newEnvironment = new NitrinoParticleNebulae(whales, segmentLength);
-                        Segments.Add(newEnvironment);
-                        break;
-                    default:
-                        throw new ArgumentException("Unknown environment type");
-                }
-            }
-        }
+        Segments.Add(environment);
     }
 }
