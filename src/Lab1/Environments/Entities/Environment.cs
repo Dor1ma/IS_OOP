a5b9874d6b9305d2ddbe71c1a063e5edeb3eceac
Pavel <admaikinpavel@yandex.ru>
2023-10-04 21:57:30 +0300

    refactor: refactor Environment.cs

--- src/Lab1/Environments/Entities/Environment.cs
+++ src/Lab1/Environments/Entities/Environment.cs
@@ -4,11 +4,11 @@ namespace Itmo.ObjectOrientedProgramming.Lab1.Environments.Entities;
 
 public abstract class Environment
 {
-    public Obstacle? FirstObstacle { get; set; }
-    public Obstacle? SecondObstacle { get; set; }
-    public int FirstObstaclesCount { get; set; }
-    public int SecondObstaclesCount { get; set; }
-    public string EngineRequired { get; set; } = "-";
-    public string ExtraRequirenment { get; set; } = "-";
-    public int EnvironmentLength { get; set; } = 100;
+    public Obstacle? FirstObstacle { get; protected init; }
+    public Obstacle? SecondObstacle { get; protected init; }
+    public int FirstObstaclesCount { get; protected init; }
+    public int SecondObstaclesCount { get; protected init; }
+    public string EngineRequired { get; protected init; } = "-";
+    public string ExtraRequirenment { get; protected init; } = "-";
+    public int EnvironmentLength { get; protected init; } = 100;
 }
