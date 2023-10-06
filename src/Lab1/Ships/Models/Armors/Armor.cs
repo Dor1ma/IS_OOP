f03ed9eeccb3ee32f7e5944c339a222096313406
Pavel <admaikinpavel@yandex.ru>
2023-10-06 19:37:54 +0300

    fix: fix new armors and deflectors

--- src/Lab1/Ships/Models/Armors/Armor.cs
+++ src/Lab1/Ships/Models/Armors/Armor.cs
@@ -1,4 +1,4 @@
-namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Models;
+namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.Armors;
 
 public abstract class Armor
 {
--- src/Lab1/Ships/Models/Deflectors/DeflectorClassTwo.cs
+++ src/Lab1/Ships/Models/Deflectors/DeflectorClassTwo.cs
@@ -1,4 +1,4 @@
-namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Models;
+namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.Deflectors;
 
 public class DeflectorClassTwo : Deflector
 {
