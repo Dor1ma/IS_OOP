f8f205c67fe84c52f762ae794ace049cfe252b58
Pavel <admaikinpavel@yandex.ru>
2023-10-10 12:51:00 +0300

    refactor: change Collection to ICollection

--- src/Lab1/Environments/Entities/Environment.cs
+++ src/Lab1/Environments/Entities/Environment.cs
@@ -1,13 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using Itmo.ObjectOrientedProgramming.Lab1.Environments.Models;
 
 namespace Itmo.ObjectOrientedProgramming.Lab1.Environments.Entities;
 
 public abstract class Environment
 {
-    public Collection<IObstacle> Obstacles { get; } = new(new List<IObstacle>());
+    public ICollection<IObstacle> Obstacles { get; } = new List<IObstacle>();
 
     public int EnvironmentLength { get; protected init; }
     public Type? Requirement { get; protected init; }
