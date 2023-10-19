5d8ff149fa6e2e86356f681eabad7ef4d484bd30
Pavel <admaikinpavel@yandex.ru>
2023-10-19 22:36:54 +0300

    feat: add clone and change methods to pc cases

--- src/Lab2/PCSetup/Models/Cases/Factories/PcCaseFactory.cs
+++ src/Lab2/PCSetup/Models/Cases/Factories/PcCaseFactory.cs
@@ -6,13 +6,13 @@ namespace Itmo.ObjectOrientedProgramming.Lab2.PCSetup.Models.Cases.Factories;
 public class PcCaseFactory
 {
     private readonly int _maximumVideoCardLength;
-    private readonly ICollection<IMotherBoard> _supportedFormFactors;
+    private readonly ICollection<MotherBoard> _supportedFormFactors;
     private readonly int _maximumCoolerHeight;
     private readonly string _name;
     public PcCaseFactory(
         string name,
         int maximumVideoCardLength,
-        ICollection<IMotherBoard> supportedFormFactors,
+        ICollection<MotherBoard> supportedFormFactors,
         int maximumCoolerHeight)
     {
         _name = name;
--- src/Lab2/PCSetup/Models/Cases/PcCase.cs
+++ src/Lab2/PCSetup/Models/Cases/PcCase.cs
@@ -5,7 +5,7 @@ namespace Itmo.ObjectOrientedProgramming.Lab2.PCSetup.Models.Cases;
 
 public class PcCase : IPcComponent
 {
-    public PcCase(string name, int maximumVideoCardLength, ICollection<IMotherBoard> supportedFormFactors, int maximumCoolerHeight)
+    public PcCase(string name, int maximumVideoCardLength, ICollection<MotherBoard> supportedFormFactors, int maximumCoolerHeight)
     {
         Name = name;
         MaximumVideoCardLength = maximumVideoCardLength;
@@ -13,8 +13,41 @@ public class PcCase : IPcComponent
         MaximumCoolerHeight = maximumCoolerHeight;
     }
 
-    public int MaximumVideoCardLength { get; }
-    public ICollection<IMotherBoard> SupportedFormFactors { get; }
-    public int MaximumCoolerHeight { get; }
-    public string Name { get; }
+    public string Name { get; private set; }
+    public int MaximumVideoCardLength { get; private set; }
+    public ICollection<MotherBoard> SupportedFormFactors { get; private set; }
+    public int MaximumCoolerHeight { get; private set; }
+
+    public PcCase Clone()
+    {
+        return new PcCase((string)Name.Clone(), MaximumVideoCardLength, SupportedFormFactors, MaximumCoolerHeight);
+    }
+
+    public PcCase ChangeName(string newName)
+    {
+        PcCase clone = Clone();
+        clone.Name = newName;
+        return clone;
+    }
+
+    public PcCase ChangeMaximumVideoCardLength(int newVideoCardLength)
+    {
+        PcCase clone = Clone();
+        clone.MaximumVideoCardLength = newVideoCardLength;
+        return clone;
+    }
+
+    public PcCase ChangeSupportedFormFactors(ICollection<MotherBoard> newSupportedFormFactors)
+    {
+        PcCase clone = Clone();
+        clone.SupportedFormFactors = newSupportedFormFactors;
+        return clone;
+    }
+
+    public PcCase ChangeMaximumCoolerHeight(int newMaximumCoolerHeight)
+    {
+        PcCase clone = Clone();
+        clone.MaximumCoolerHeight = newMaximumCoolerHeight;
+        return clone;
+    }
 }
