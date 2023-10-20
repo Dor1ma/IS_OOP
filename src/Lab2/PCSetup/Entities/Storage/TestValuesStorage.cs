defde22da1ad90804b0a0a35971e3baf338afaa1
Pavel <admaikinpavel@yandex.ru>
2023-10-20 20:19:52 +0300

    fix: fix TestValuesStorage.cs

--- src/Lab2/PCSetup/Entities/Storage/TestValuesStorage.cs
+++ src/Lab2/PCSetup/Entities/Storage/TestValuesStorage.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Itmo.ObjectOrientedProgramming.Lab2.PCSetup.Models;
+using Itmo.ObjectOrientedProgramming.Lab2.PCSetup.Models.BiosDirectory;
 using Itmo.ObjectOrientedProgramming.Lab2.PCSetup.Models.Cases;
 using Itmo.ObjectOrientedProgramming.Lab2.PCSetup.Models.Chipsets;
 using Itmo.ObjectOrientedProgramming.Lab2.PCSetup.Models.Chipsets.AMD;
@@ -26,30 +27,42 @@ public class TestValuesStorage : IStorage
     private readonly int _firstProcessorMaximumDdrFrequency = 3200;
     private readonly int _firstProcessorTdp = 65;
     private readonly int _firstProcessorPowerConsumption = 65;
+
     private readonly string _secondProcessorName = "Intel Core I5-12400F";
     private readonly double _secondProcessorCoreFrequency = 2.5;
     private readonly int _secondProcessorCoreCount = 6;
     private readonly int _secondProcessorMaximumDdrFrequency = 4200;
     private readonly int _secondProcessorTdp = 117;
     private readonly int _secondProcessorPowerConsumption = 117;
+
     private readonly string _amdMotherBoardName = "MSI B450 Gaming Plus Max";
     private readonly int _b450MaximumDdrFrequency = 4133;
+    private readonly string _amdBiosName = "I am AMD bios";
+    private readonly int _biosVersion = 1;
+    private readonly ICollection<Processor> _amdBiosSupportableProcessors = new List<Processor>();
     private readonly bool _isXmpSupported = true;
     private readonly int _specialInitValue = 1;
     private readonly IRamType _amdMotherBoardDdrType = new DdrFiveRam();
+
     private readonly string _intelMotherBoardName = "GIGABYTE B760 DS3H";
     private readonly int _b760MaximumDdrFrequency = 5333;
     private readonly IRamType _intelMotherBoardDdrType = new DdrFiveRam();
+    private readonly string _intelBiosName = "I am Intel bios";
+    private readonly ICollection<Processor> _intelBiosSupportableProcessors = new List<Processor>();
+
     private readonly string _powerSupplyName = "Be quite! Pure Power 11";
     private readonly int _powerSupplyPowerConsumption = 600;
+
     private readonly string _ssdName = "Kingston A400";
     private readonly int _ssdMemorySize = 240;
     private readonly int _ssdPowerConsumption = 2;
     private readonly int _ssdOperationSpeed = 500;
+
     private readonly string _ramName = "G.Skill TRIDENT Z5";
     private readonly int _ramMemorySize = 32;
     private readonly int _ramPowerConsumption = 2;
     private readonly IRamType _ramType = new DdrFiveRam();
+
     private readonly string _gpuName = "MSI GeForce RTX 4060 Gaming X";
     private readonly int _gpuLength = 247;
     private readonly int _gpuWidth = 130;
@@ -57,10 +70,13 @@ public class TestValuesStorage : IStorage
     private readonly double _gpuPciExpressVersion = 4.0;
     private readonly int _gpuChipFrequency = 1830;
     private readonly int _gpuPowerConsumption = 115;
+
     private readonly string _airCollerName = "Be quite! Pure Rock 2";
     private readonly int _airCollerHeight = 155;
     private readonly int _airCollerMaximumTdp = 150;
     private readonly ICollection<Processor> _aircollerSupportableSockets = new List<Processor>();
+    private readonly int _airCoolerPowerConsumption = 12;
+
     private readonly string _pcCaseName = "LIAN LI Lancool 205 Mesh White";
     private readonly int _pcCaseMaximumVideoCardLength = 350;
     private readonly ICollection<MotherBoard> _pcCaseSupportableFormFactors = new List<MotherBoard>();
@@ -111,16 +127,41 @@ public class TestValuesStorage : IStorage
             _specialInitValue);
 
         // Base motherboards initialization
+        _amdBiosSupportableProcessors.Add(
+            new Am4Processor(
+                _firstProcessorName,
+                _firstProcessorCoreFrequency,
+                _firstProcessorCoreCount,
+                firstProcessorIntegratedVideoCore,
+                _firstProcessorMaximumDdrFrequency,
+                _firstProcessorTdp,
+                _firstProcessorPowerConsumption));
+
+        Bios amdBios = new BiosTypeOne(_amdBiosName, _biosVersion, _amdBiosSupportableProcessors);
         _components.Add(new AtxMotherBoard(
             _amdMotherBoardName,
             amdMotherBoardSocket,
             b450Chipset,
-            _amdMotherBoardDdrType));
+            _amdMotherBoardDdrType,
+            amdBios));
+
+        _intelBiosSupportableProcessors.Add(
+            new Lga1700Processor(
+                initSocketName,
+                _specialInitValue,
+                _specialInitValue,
+                initSocketGpu,
+                _specialInitValue,
+                _specialInitValue,
+                _specialInitValue));
+
+        Bios intelBios = new BiosTypeOne(_intelBiosName, _biosVersion, _intelBiosSupportableProcessors);
         _components.Add(new AtxMotherBoard(
             _intelMotherBoardName,
             intelMotherBoardSocket,
             b760Chipset,
-            _intelMotherBoardDdrType));
+            _intelMotherBoardDdrType,
+            intelBios));
 
         // Base power supply initialization
         _components.Add(new PowerSupply(_powerSupplyName, _powerSupplyPowerConsumption));
@@ -158,24 +199,27 @@ public class TestValuesStorage : IStorage
             _secondProcessorMaximumDdrFrequency,
             _secondProcessorTdp,
             _secondProcessorPowerConsumption));
-        _components.Add(new Cooler(_airCollerName, _airCollerHeight, _airCollerMaximumTdp, _aircollerSupportableSockets));
+        _components.Add(new Cooler(_airCollerName, _airCollerHeight, _airCollerMaximumTdp, _aircollerSupportableSockets, _airCoolerPowerConsumption));
 
         // Base pc case initialization
         _pcCaseSupportableFormFactors.Add(new AtxMotherBoard(
             _amdMotherBoardName,
             amdMotherBoardSocket,
             b450Chipset,
-            _amdMotherBoardDdrType));
+            _amdMotherBoardDdrType,
+            amdBios));
         _pcCaseSupportableFormFactors.Add(new MiniAtxBoard(
             _amdMotherBoardName,
             amdMotherBoardSocket,
             b450Chipset,
-            _amdMotherBoardDdrType));
+            _amdMotherBoardDdrType,
+            amdBios));
         _pcCaseSupportableFormFactors.Add(new MicroAtxBoard(
             _amdMotherBoardName,
             amdMotherBoardSocket,
             b450Chipset,
-            _amdMotherBoardDdrType));
+            _amdMotherBoardDdrType,
+            amdBios));
         _components.Add(new PcCase(_pcCaseName, _pcCaseMaximumVideoCardLength, _pcCaseSupportableFormFactors, _pcCaseMaximumCoolerHeight));
     }
 
