0975802ef67f6ff4e7931bdebb546d8e0d38144e
Pavel <admaikinpavel@yandex.ru>
2023-11-03 10:51:11 +0300

    feat: create mock for Messenger

---
+++ src/Lab3/Models/Addressee/MockAddresseeMessenger.cs
@@ -0,0 +1,37 @@
+using Itmo.ObjectOrientedProgramming.Lab3.Entities;
+using Itmo.ObjectOrientedProgramming.Lab3.Models.MessageEndPoints;
+using Itmo.ObjectOrientedProgramming.Lab3.Services;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.Models.Addressee;
+
+public class MockAddresseeMessenger : IAddressee
+{
+    private readonly ILogger _logger;
+    private PriorityLevels _filter = PriorityLevels.None;
+
+    public MockAddresseeMessenger(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public IMessageEndPoint ConcreteAddressee { get; } = new MockMessenger();
+
+    public void Receive(Message message)
+    {
+        if (_filter == PriorityLevels.None || message.PriorityLevel == _filter)
+        {
+            ConcreteAddressee.Save(message);
+            _logger.LogInformation($"Messenger received its message: {message.Body}");
+        }
+        else
+        {
+            _logger.LogInformation($"Messenger didn't receive message: {message.Body}" +
+                                   $"Reason: filter mismatch");
+        }
+    }
+
+    public void SetupFilter(PriorityLevels priorityLevel)
+    {
+        _filter = priorityLevel;
+    }
+}
--- src/Lab3/Models/MessageEndPoints/MockMessenger.cs
+++ src/Lab3/Models/MessageEndPoints/MockMessenger.cs
@@ -4,12 +4,15 @@ using Itmo.ObjectOrientedProgramming.Lab3.Entities;
 
 namespace Itmo.ObjectOrientedProgramming.Lab3.Models.MessageEndPoints;
 
-public class Messenger : IMessageEndPoint
+public class MockMessenger : IMessageEndPoint
 {
     private const string Postscript = " MESSENGER";
+    public string? ShowingResult { get; private set; }
     public ICollection<Message> Messages { get; } = new List<Message>();
-    public static void ShowText(Message message)
+
+    public void ShowText(Message message)
     {
+        ShowingResult = $"{message.Body}" + Postscript;
         Console.WriteLine($"{message.Body}" + Postscript);
     }
 
