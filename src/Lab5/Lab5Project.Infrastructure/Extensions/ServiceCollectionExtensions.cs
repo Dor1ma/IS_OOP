f33a843e44a83320fd95b69cb4e0c0fe43ac1b17
Pavel <admaikinpavel@yandex.ru>
2023-12-13 20:47:27 +0300

    feat: update extensions in infrastructure layer

--- src/Lab5/Lab5Project.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Lab5/Lab5Project.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 using Itmo.Dev.Platform.Postgres.Extensions;
 using Itmo.Dev.Platform.Postgres.Models;
+using Itmo.Dev.Platform.Postgres.Plugins;
 using Lab5Project.Application.Abstractions.Repositories;
+using Lab5Project.Infrastructure.Plugins;
 using Lab5Project.Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,7 +16,12 @@ public static class ServiceCollectionExtensions
     {
         collection.AddPlatformPostgres(builder => builder.Configure(configuration));
         collection.AddPlatformMigrations(typeof(ServiceCollectionExtensions).Assembly);
+
+        collection.AddSingleton<IDataSourcePlugin, MappingPlugin>();
+
         collection.AddScoped<IUsersRepository, UsersRepository>();
+        collection.AddScoped<IAdminsRepository, AdminsRepository>();
+        collection.AddScoped<IOperationsHistoryRepository, OperationsHistoryRepository>();
 
         return collection;
     }
